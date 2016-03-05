e7e21cdd7ff83f4fa8583c8eb9ae1bb47adff7bd
Matt Augustine <matt@playfab.com>
2016-03-04 18:49:19 -0800

    Update initial.js

--- initial.js
+++ initial.js
@@ -1,13 +1,13 @@
 // Test various succes and error conditions
 
 handlers.helloWorld = function (args) {
-    var message = "Hello " + currentPlayerId + "!";
+    var message = 'Hello ' + currentPlayerId + '!';
     log.info(message);
     return { messageValue: message };
 }
 
 handlers.scriptException = function (args) {
-    log.info("before the error");
+    log.info('before the error');
     var invalid = doesnotexist.yet;
 }
 
@@ -18,13 +18,13 @@ handlers.playFabAPIBadRequest = function (args) {
 }
 
 handlers.httpOkRequest = function (args) {
-    var response = http.request("https://aaa.playfabapi.com/HealthStatus");
-    log.debug("received: " + response);
+    var response = http.request('https://aaa.playfabapi.com/HealthStatus');
+    log.debug('received: ' + response);
 }
 
 handlers.httpNotFoundRequest = function (args) {
-    var response = http.request("http://httpbin.org/status/404");
-    log.debug("received: " + response);
+    var response = http.request('http://httpbin.org/status/404');
+    log.debug('received: ' + response);
 }
 
 handlers.httpInternalServerErrorRequest = function (args) {
